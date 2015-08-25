70c9388ffc66d47f6338656b8af18da8485e7997
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-08-25 16:01:04 +0200

    initial version

---
+++ src/createComponent.js
@@ -0,0 +1,4 @@
+module.exports = function (React) {
+  class Component extends React.Component {}
+  return Component;
+};
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const createComponent = require("./createComponent");
+const createShaders = require("./createShaders");
+const createTarget = require("./createTarget");
+const createView = require("./createView");
+
+module.exports = {
+  createComponent,
+  createShaders,
+  createTarget,
+  createView
+};
